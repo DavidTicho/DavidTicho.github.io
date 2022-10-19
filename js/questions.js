226f65ba46afd788b9f8144c2f7f9979ae8fffb2
David <114966070+DavidTicho@users.noreply.github.com>
2022-10-18 22:22:37 -0400

    Add files via upload

---
+++ js/questions.js
@@ -0,0 +1,69 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+    {
+    numb: 1,
+    question: "Simbolo de Azufre",
+    answer: "S",
+    options: ["As<sub>2</sub>","Se","Az","S"]
+  },
+    {
+    numb: 2,
+    question: "Simbolo de Hierro",
+    answer: "Fe",
+    options: ["Feo","He","Fe","Hi"]
+  },
+    {
+    numb: 3,
+    question: "Simbolo de Fosforo",
+    answer: "P",
+    options: ["F","P","Fo","K"]
+  },
+    {
+    numb: 4,
+    question: "Simbolo de Teluro",
+    answer: "Te",
+    options: ["T","Tu","Te","Tl"]
+  },
+    {
+    numb: 5,
+    question: "Simbolo de Oro",
+    answer: "Au",
+    options: ["Ae","Au","Ou","Ea"]
+  },
+  {
+    numb: 6,
+    question: "Simbolo de Cloro",
+    answer: "Cl",
+    options: ["Cl","Co","Cr","C"]
+  },
+  {
+    numb: 7,
+    question: "Simbolo de Magnesio",
+    answer: "Mg",
+    options: ["Ma","Mc","Mg","Mh"]
+  },
+  {
+    numb: 8,
+    question: "Simbolo de Cromo",
+    answer: "Cr",
+    options: ["C","Co","Cu","Cr"]
+  },
+  {
+    numb: 9,
+    question: "Simbolo de Manganeso",
+    answer: "Mn",
+    options: ["Mg","Ma","Mn","Mo"]
+  },
+  {
+    numb: 10,
+    question: "Simbolo de Cobre",
+    answer: "Cu",
+    options: ["C","Cu","Co","Cb"]
+  },
+  {
+    numb: 11,
+    question: "Simbolo de Cobalto",
+    answer: "Co",
+    options: ["Ca","Co","Cu","Cb"]
+  },
+];
